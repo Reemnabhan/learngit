9436a27a3417eeeb1d44b2489de45976b09db42a
Reemnabhan <98900968+Reemnabhan@users.noreply.github.com>
2022-03-16 17:11:08 +0100

    my second commit

--- src/be/brussel/App.java
+++ src/be/brussel/App.java
@@ -3,6 +3,9 @@ package be.brussel;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println()
+        System.out.println();
+
+                System.out.println();
+
     }
 }
